a935d10179c1bd71b39f92aac9beb072453eff80
Angela Sofia Osorio <angiemeow89@gmail.com>
2023-09-25 14:04:41 -0600

    Update server.js

--- api/server.js
+++ api/server.js
@@ -15,5 +15,31 @@ server.listen(3000, () => {
     console.log('JSON Server is running')
 })
 
+
+
+// Ruta para actualizar un recurso por su ID
+server.put('/api/resource/:id', (req, res) => {
+  const resourceId = parseInt(req.params.id);
+  const updatedData = req.body; // Datos actualizados en el cuerpo de la solicitud
+
+  // Realiza la actualización en tu archivo db.json o base de datos simulada
+  // Debes encontrar el objeto con el ID correspondiente y actualizar sus propiedades con los datos actualizados
+
+  // Envía una respuesta adecuada, por ejemplo, el objeto actualizado
+  res.json(updatedData);
+});
+
+// Ruta para eliminar un recurso por su ID
+server.delete('/api/resource/:id', (req, res) => {
+  const resourceId = parseInt(req.params.id);
+
+  // Realiza la eliminación en tu archivo db.json o base de datos simulada
+  // Debes encontrar y eliminar el objeto con el ID correspondiente
+
+  // Envía una respuesta adecuada, por ejemplo, un mensaje de éxito
+  res.json({ message: 'Registro eliminado con éxito' });
+});
+
+
 // Export the Server API
 module.exports = server
